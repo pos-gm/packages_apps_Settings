e22e4681045e84fbf42bf7555d30749f82cfabcc
spkal01 <kalligeross@gmail.com>
2022-05-01 15:37:31 +0300

    Settings: Remove refresh rate category on unsupported devices
    [ghostrider-reborn: simplify and clean up]
    
    Change-Id: I55d963a2ab59427acd688cd5a522c821a89687bf
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -51,6 +51,7 @@ public class DisplaySettings extends DashboardFragment {
 
     private static final String KEY_HIGH_TOUCH_POLLING_RATE = "high_touch_polling_rate_enable";
     private static final String KEY_HIGH_TOUCH_SENSITIVITY = "high_touch_sensitivity_enable";
+    private static final String KEY_REFRESH_RATE_CATEGORY = "refresh_rate_category";
 
     @Override
     public int getMetricsCategory() {
@@ -70,6 +71,10 @@ public class DisplaySettings extends DashboardFragment {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+
+        if (!getActivity().getResources().getBoolean(R.bool.config_show_smooth_display)) {
+            getPreferenceScreen().removePreference(findPreference(KEY_REFRESH_RATE_CATEGORY));
+        }
     }
 
     @Override
